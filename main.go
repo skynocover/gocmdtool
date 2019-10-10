2c81f1706b0d16b2c6aa323f20360ad6e11d67c6
eric <skynocover@Gmail.com>
2019-10-10 14:56:13 +0800

    0.1.0

---
+++ main.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+func CallClear() { //清空用
+	var clear map[string]func()     //create a map for storing clear funcs
+	clear = make(map[string]func()) //Initialize it
+	clear["linux"] = func() {
+		cmd := exec.Command("clear") //Linux example, its tested
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+	clear["windows"] = func() {
+		cmd := exec.Command("cmd", "/c", "cls") //Windows example, its tested
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+	value, ok := clear[runtime.GOOS] //runtime.GOOS -> linux, windows, darwin etc.
+	if ok {                          //if we defined a clear func for that platform:
+		value() //we execute it
+	} else { //unsupported platform
+		panic("Your platform is unsupported! I can't clear terminal screen :(")
+	}
+}
